7c081e25675c02da45a4f680898a56027d922599
Javasrabni <javasrabnii@gmail.com>
2026-04-09 04:08:21 +0700

    fix: error vercel

--- app/adm/components/All-Admin/allAdmin.tsx
+++ app/adm/components/All-Admin/allAdmin.tsx
@@ -9,13 +9,13 @@ const allAdmin = async () => {
 
     return (
         <div className="flex flex-col gap-8">
-            {allAdminData.filter(adm => adm.risenttaID !== you.risenttaID).map((adm) =>
-                <div key={adm.risenttaID}>
+            {allAdminData.filter(adm => adm.risentaID !== you.risentaID).map((adm) =>
+                <div key={adm.risentaID}>
                     <div className='flex flex-row gap-4 items-center '>
                         <div className='relative w-12 h-12 rounded-full text-white'>
                             <Image src={adm.photoProfile || `/Assets/team/${adm.adm_usn.replaceAll(' ', '')}.jpeg`} alt={`${adm.adm_usn} Photo`} fill className="object-cover rounded-full" />
                         </div>
-                        <Link href={`/adm/profile?user=${adm.risenttaID}`} className="hover:underline hover:text-blue-600 dark:hover:text-blue-400 transition-colors">
+                        <Link href={`/adm/profile?user=${adm.risentaID}`} className="hover:underline hover:text-blue-600 dark:hover:text-blue-400 transition-colors">
                             <p>{adm.adm_usn}</p>
                         </Link>
                     </div>
